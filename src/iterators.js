d328f1d6f91e2ff018cf8ea118f92d8a7f5e5544
Thomas Rampelberg <trampelb@grue.612howard.bit>
2010-03-22 10:59:18 -0700

    Working iterator implementation.

--- src/iterators.js
+++ src/iterators.js
@@ -13,11 +13,18 @@ var iterator = function(iterable) { return new iterator.fn.init(iterable); }
 iterator.fn = iterator.prototype = {
   init: function(iterable) {
     this.i = 0;
+    this.iterable = iterable;
+    if (typeof(StopIteration) != "undefined")
+      this.stop_iteration = StopIteration;
+    else {
+      this.stop_iteration = new Error('StopIteration');
+      this.stop_iteration.name = 'StopIteration';
+    }
     return this;
   },
   next: function() {
     var self = this;
-    if (i >= self.iterable.length)
+    if (self.i >= self.iterable.length)
       throw self.stop_iteration;
     return self.iterable[self.i++];
   }
