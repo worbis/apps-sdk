6e4866d8d959a9d9947613d78332355648380d86
Thomas Rampelberg <trampelb@grue.612howard.bit>
2010-03-25 16:28:38 -0700

    Trying to make the simulator its own package with it's own dependencies

---
+++ src/dependencies.js
@@ -0,0 +1,13 @@
+/*
+ * btapp dependencies
+ * Copyright (c) 2010 BitTorrent Inc.
+ *
+ * Date: %date%
+ * Revision: %revision%
+ */
+
+var tags = [
+  { }
+];
+
+var jsloader = new JSLoad(tags, "src/");
