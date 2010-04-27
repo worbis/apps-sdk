1594ca2bdb511f73fd089551654cea8502c748a3
Thomas Rampelberg <trampelb@grue.612howard.bit>
2010-04-26 18:09:24 -0700

    Trying to get cookie storage to work like the stash. So far, it's not as easy as it looks.

--- src/bt.js
+++ src/bt.js
@@ -10,7 +10,25 @@
  */
 
 var bt = {
-  
+  stash: {
+    all: function() {
+      var everything = btapp.stash.all();
+      _.each(everything, function(v, k) {
+        everything[k] = JSON.parse(v);
+      });
+      return everything;
+    },
+    keys: function() { return btapp.stash.keys() },
+    get: function(k, d) {
+      try {
+        return JSON.parse(btapp.stash.get(k));
+      } catch(err) {
+        if (d == null && d !== null)
+          throw err;
+        return d;
+      } },
+    set: function(k, v) { btapp.stash.set(k, JSON.stringify(v)); }
+  }
 };
 
 (function() {
@@ -20,7 +38,18 @@ var bt = {
 
   window.btapp = {
     stash: {
-      all: function() { return btapp.stash._data }
-    },
+      _set_cookie: function(key, value) {
+        document.cookie = key + '=' + value + '; path=/';
+      },
+      _get_cookies: function() {
+        _(document.cookie.split(';')).chain().map(function(v) { 
+          return v.split('='); }).reduce({}, function(acc, val) { 
+            acc[val[0]] = val[1]; return acc }).value();
+      },
+      all: function() { return btapp.stash._get_cookies(); },
+      keys: function() { return _.keys(btapp.stash.all()) },
+      get: function(k) { return btapp.stash.all()[k]; },
+      set: function(k, v) { btapp.stash._set_cookie(k, v); }
+    }
   };
 })();
