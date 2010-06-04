d3d9dae2031163ca3515d98454d5fca92d135cd8
Thomas Rampelberg <thomas@bittorrent.com>
2010-06-04 14:00:11 -0700

    Making build/ work with the server command.

--- griffin/command/serve.py
+++ griffin/command/serve.py
@@ -24,11 +24,57 @@ class GriffinRequests(SimpleHTTPServer.SimpleHTTPRequestHandler):
         return SimpleHTTPServer.SimpleHTTPRequestHandler.translate_path(
             self, path)
 
-    def do_POST(req):
-        fp = open(os.path.join('test', req.path[1:]), 'wb')
-        fp.write(req.rfile.read(int(req.headers['Content-Length'])))
+    def send_head(self):
+        # Special version of send_head that falls back to the build directory.
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            if not self.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(301)
+                self.send_header("Location", self.path + "/")
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            # Always read in binary mode. Opening files in text mode may cause
+            # newline translations, making the actual size of the content
+            # transmitted *less* than the content-length!
+            f = open(path, 'rb')
+        except IOError:
+            # XXX - Modification, don't send the error here.
+            return None
+        self.send_response(200)
+        self.send_header("Content-type", ctype)
+        fs = os.fstat(f.fileno())
+        self.send_header("Content-Length", str(fs[6]))
+        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+        self.end_headers()
+        return f
+
+    def do_GET(self):
+        f = self.send_head()
+        if not f:
+            self.path = '/build' + self.path
+            f = self.send_head()
+        if not f:
+            self.send_error(404, 'File not found')
+        if f:
+            self.copyfile(f, self.wfile)
+            f.close()
+
+    def do_POST(self):
+        fp = open(os.path.join('test', self.path[1:]), 'wb')
+        fp.write(self.rfile.read(int(self.headers['Content-Length'])))
         fp.close()
-        req.send_response(200)
+        self.send_response(200)
 
 class serve(griffin.command.base.Command):
 
