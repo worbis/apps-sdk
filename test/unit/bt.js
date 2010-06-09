e52306c10e92777fb18e8462a0f320eb80a0addb
Thomas Rampelberg <thomas@bittorrent.com>
2010-06-09 15:19:06 -0700

    finishing off the unit test coverage for now. Also, added a new feature to bt.add.torrent (url, defaults, cb)

--- test/unit/bt.js
+++ test/unit/bt.js
@@ -9,12 +9,19 @@
 module('bt');
 
 test('bt.add.torrent', function() {
-  expect(4);
+  expect(9);
 
   var url = 'http://example.com/add.torrent';
+  var url_nocb = 'http://example.com/cb.torrent';
+  var url_def = 'http://example.com/def.torrent';
+  var url_cbdef = 'http://example.com/cbdef.torrent';
+  var defs = { label: 'foobar',
+               name: 'foobar' };
   // Just in case.
   bt.events.set('torrent', bt._handlers.torrent);
   stop();
+  bt.add.torrent(url_nocb);
+  bt.add.torrent(url_def, defs);
   bt.add.torrent(url, function(resp) {
     equals(resp.url, url, 'Url\'s set right');
     equals(resp.status, 200, 'Status is okay');
@@ -23,12 +30,24 @@ test('bt.add.torrent', function() {
       return v.properties.get('download_url');
     });
     ok(download_urls.indexOf(url) >= 0, 'Torrent added successfully');
-    start();
+
+    ok(download_urls.indexOf(url_nocb) >= 0, 'No cb or defaults added okay');
+    var tor = bt.torrent.get(url_def);
+    _.each(defs, function(v, k) {
+      equals(tor.properties.get(k), v, 'Defaults are set');
+    });
+    bt.add.torrent(url_cbdef, defs, function(resp) {
+      var tor = bt.torrent.get(url_cbdef);
+      _.each(defs, function(v, k) {
+        equals(tor.properties.get(k), v, 'Callback + defaults works');
+      });
+      start();
+    });
   });
 });
 
 test('bt.stash', function() {
-  expect();
+  expect(8);
 
   var objs = { foo: 'bar',
               bar: [1, 2, '3'],
@@ -44,19 +63,30 @@ test('bt.stash', function() {
 });
 
 test('bt.events', function() {
-  expect();
+  expect(3);
 
-
+  var fn = function() { };
+  bt.events.set('torrent', fn);
+  same(bt.events.get('torrent'), fn, 'Callback set correctly');
+  ok(bt.events.keys().indexOf('torrent') >= 0, 'Torrent shows up in the keys');
+  same(bt.events.all()['torrent'], fn, 'All is returning the right data');
 });
 
 test('bt.torrent', function() {
-  expect();
+  expect(4);
 
-
+  var url = 'http://example.com/add.torrent';
+  var tor = bt.torrent.get(url);
+  equals(tor.properties.get('download_url'), url, 'Got the right torrent');
+  equals(bt.torrent.get(tor.hash).properties.get('hash'), tor.hash,
+         'Can get by hash too');
+  ok(bt.torrent.keys().indexOf(tor.hash) >= 0, 'Keys has the right hashes');
+  same(bt.torrent.all()[tor.hash], tor, 'all() has at least one right key');
 });
 
 test('bt.resource', function() {
-  expect();
+  expect(1);
 
-
+  var txt = 'test123\n';
+  equal(bt.resource('data/foobar'), txt, 'Fetched the right data');
 });
