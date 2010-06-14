150618b030cc63e96ce2032660a2c7845e2ec9ff
Thomas Rampelberg <thomas@bittorrent.com>
2010-06-14 13:36:39 -0700

    Adding the griffin egg in.

--- loader/setup.py
+++ loader/setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup
 import py2exe
 
-setup(console = ['loader.py'])
+setup(console = ['loader.py'],
+      data_files = [('.', ['../dist/griffin-0.1-py2.6.egg'])]
+      )
