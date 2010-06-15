d414eb65e3877457fa56e7b6d1f729b9e144ec1b
Thomas Rampelberg <thomas@saunter.org>
2010-06-14 18:11:58 -0700

    Finishing off py2exe, now just need to test it

--- loader/setup.py
+++ loader/setup.py
@@ -1,22 +1,12 @@
 from distutils.core import setup
 import py2exe
-import modulefinder
 
-modulefinder.AddPackagePath("email.mime", "base")
-modulefinder.AddPackagePath("email.mime", "multipart")
-modulefinder.AddPackagePath("email.mime", "nonmultipart")
-modulefinder.AddPackagePath("email.mime", "audio")
-modulefinder.AddPackagePath("email.mime", "image")
-modulefinder.AddPackagePath("email.mime", "message")
-modulefinder.AddPackagePath("email.mime", "application")
-
-
-setup(console = ['loader.py'],
+setup(console = ['apps.py'],
       data_files = [('.', ['../dist/griffin-0.1-py2.6.egg'])],
       options = { 'py2exe': { "unbuffered": True,
                               "optimize": 2,
                               "includes": [ 'email' ],
-                              "packages": ["mako.cache"],
+                              "packages": ["mako.cache", "email"],
                               }
                 }
       )
