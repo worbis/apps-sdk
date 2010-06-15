6b07f3b92760872f1c5009684405caba93a95e2f
Thomas Rampelberg <thomas@saunter.org>
2010-06-15 15:52:08 -0700

    Loader is breaking because of name similarities

--- build.bash
+++ build.bash
@@ -1,6 +1,8 @@
 python setup.py develop
 python setup.py bdist_egg
 cd loader/
+rm -rf dist/
 python setup.py py2exe
+mv dist/apps-sdk.exe dist/apps.exe
 python lib.py
 PYTHONPATH=dist/ easy_install-2.6 -d dist -zUax ../dist/*.egg
