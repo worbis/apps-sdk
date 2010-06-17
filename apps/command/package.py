a8cd945c1cea9eb430530f0103ecd437d5427658
Thomas Rampelberg <thomas@bittorrent.com>
2010-06-17 11:14:58 -0700

    Removing the special ignore logic from package since it is in base now.

--- apps/command/package.py
+++ apps/command/package.py
@@ -19,28 +19,19 @@ class package(apps.command.base.Command):
 
     def run(self):
         path = self.options['path']
-        ignore = [os.path.join(self.project.path, x) for x in
-                  open(os.path.join(self.project.path,
-                                    '.ignore')).read().split('\n')]
-        def _filter(fname):
-            for pat in ignore:
-                if fnmatch.fnmatch(fname, pat):
-                    return False
-            return True
         try:
             os.makedirs(path)
         except:
             pass
         btapp = zipfile.ZipFile(open(os.path.join(path, '%s.btapp' % (
                     self.project.metadata['name'],)), 'wb'), 'w')
-        for p, dirs, files in os.walk(self.project.path):
-            for f in filter(_filter, [os.path.join(p, x) for x in files]):
-                # Files in the build/ directory are auto-created for users,
-                # they mirror the normal path and are only in the build
-                # directory to keep it out of the way of users.
-                fpath = os.path.split(f)
-                arcname = os.path.join(*fpath[1:]) if re.match('\..build',
-                                                               fpath[0]) \
-                    else os.path.join(*fpath)
-                btapp.write(f, arcname)
+        for f in self.file_list():
+            # Files in the build/ directory are auto-created for users,
+            # they mirror the normal path and are only in the build
+            # directory to keep it out of the way of users.
+            fpath = os.path.split(f)
+            arcname = os.path.join(*fpath[1:]) \
+                if re.match('\..build', fpath[0]) \
+                else os.path.join(*fpath)
+            btapp.write(f, arcname)
         btapp.close()
