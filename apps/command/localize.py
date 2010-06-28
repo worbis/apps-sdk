3e0c4fa60e05d4224edec1a63ffec3058ca98aee
Thomas Rampelberg <thomas@bittorrent.com>
2010-06-28 13:58:40 -0700

    Making everything wrap at 80 characters and removing some superfulous code

--- apps/command/localize.py
+++ apps/command/localize.py
@@ -13,46 +13,50 @@ import apps.config
 
 class localize(apps.command.base.Command):
 
-    user_options = [ 
+    user_options = [
         ('dir=', 'd', 'translation file directory', None),
         ('remove', 'r', 'remove .po files from original location', None)
                    ]
     option_defaults = { 'dir': 'lang' }
-    help = 'Generate JSON for localization from a directory of .po and .mo files.'
+    help = 'Generate JSON for localization from a directory of .po and ' \
+        '.mo files.'
 
     def run(self):
-
-        if not os.path.exists(os.path.join(self.project.path, self.options['dir'])):
-            logging.error('The directory "%s" does not exist.' % (self.options['dir'], ))
+        if not os.path.exists(os.path.join(self.project.path,
+                                           self.options['dir'])):
+            logging.error('The directory "%s" does not exist.' % (
+                    self.options['dir'], ))
             return
         path = os.path.join(self.project.path, self.options['dir'])
-        for item in os.listdir(path):  
+        for item in os.listdir(path):
             ext = os.path.splitext(item)[1]
             lang = os.path.splitext(item)[0]
-            if os.path.exists(os.path.join(path, lang))==False and (ext==".mo" or ext==".po"):
+            if os.path.exists(os.path.join(path, lang)) == False and \
+                    (ext == ".mo" or ext == ".po"):
                 os.makedirs(os.path.join(path, lang, "LC_MESSAGES"))
             if ext == ".po":
-                shutil.copy(os.path.join(path, item), os.path.join(path, lang, item))
+                shutil.copy(os.path.join(path, item),
+                            os.path.join(path, lang, item))
                 if self.options.get('remove', None):
                     os.remove(os.path.join(path, item))
             elif ext == ".mo":
-                shutil.copy(os.path.join(path, item), os.path.join(path, lang, "LC_MESSAGES", item))
+                shutil.copy(os.path.join(path, item), os.path.join(
+                        path, lang, "LC_MESSAGES", item))
                 if self.options.get('remove', None):
                     os.remove(os.path.join(path, item))
 
-                fobj = codecs.open(os.path.join(path, lang, lang+".js"), "wb", "utf-8")
+                fobj = codecs.open(os.path.join(path, lang, lang+".js"), "wb",
+                                   "utf-8")
                 tr = gettext.translation(lang, path, [lang])
-                keys = tr._catalog.keys()
-                keys.sort()
                 ret = {}
-                for k in keys:
-                    v = tr._catalog[k]
+                for k, v in tr._catalog.iteritems():
                     if type(k) is tuple:
                         if k[0] not in ret:
                             ret[k[0]] = []
                         ret[k[0]].append(v)
                     else:
                         ret[k] = v
-                fobj.write(json.dumps(ret, ensure_ascii = False, indent = False))
+                fobj.write(json.dumps(ret, ensure_ascii=False, indent=False))
                 fobj.close()
-                logging.info("Successfully processed JSON for language: "+lang)
+                logging.info("Successfully processed JSON for language: %s" % (
+                        lang,))
